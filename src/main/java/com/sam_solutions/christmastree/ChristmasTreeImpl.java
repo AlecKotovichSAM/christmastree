1969e168f96c69be5cd170825989a13815d14da5
Alec Kotovich <o.kotovich@sam-solutions.net>
2014-12-05 14:00:22 +0300

    Fixed

--- src/main/java/com/sam_solutions/christmastree/ChristmasTreeImpl.java
+++ src/main/java/com/sam_solutions/christmastree/ChristmasTreeImpl.java
@@ -3,68 +3,81 @@ package com.sam_solutions.christmastree;
 import java.util.Iterator;
 
 /**
- * User: Yahor Kastsialei
- * Date: 12/4/14
- * Time: 10:56 AM
+ * User: Yahor Kastsialei Date: 12/4/14 Time: 10:56 AM
  */
 public class ChristmasTreeImpl implements IChristmasTree {
-  public static int TREE_HEIGHT = 52;
-//  private static List<> SQC = ".~'O'~.";
-  private String newLine = System.getProperty("line.separator");
-  private Garland garland;
-  private int height;
+	public static int TREE_HEIGHT = 52;
+	// private static List<> SQC = ".~'O'~.";
+	private final String newLine = System.getProperty("line.separator");
+	private final Garland garland;
+	private int height;
 
-  public ChristmasTreeImpl(final Garland garland) {
-    this.garland = garland;
-  }
+	public ChristmasTreeImpl(final Garland garland) {
+		this.garland = garland;
+	}
 
-  @Override
-  public void build(int height) {
-    this.height = height;
+	@Override
+	public void build(int height) {
+		this.height = height;
 
-    garland.initiate();
-  }
+		garland.initiate();
+	}
 
-  @Override
-  public void fire() {
-    garland.startFiring();
-  }
+	@Override
+	public void fire() {
+		garland.startFiring();
+	}
 
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    appendSpaces(builder, height);
-    builder.append(Symbols.STAR.getPresentation());
-    appendSpaces(builder, height);
-    builder.append(newLine);
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		appendSpaces(builder, height);
+		builder.append(Symbols.STAR.getPresentation());
+		appendSpaces(builder, height);
+		builder.append(newLine);
 
-    StringBuilder garlandBuilder = new StringBuilder();
-    Iterator<ISymbol> garlandIterator = garland.iterator();
-    for(int i = height; i > 0 ; i--) {
-      garlandBuilder.append(newLine);
-      appendSpaces(garlandBuilder, height-i);
-      for(int j = height-i; j <= i+height;j++) {
-//        System.out.println("j="+j+"; i="+i + "; space="+(TREE_HEIGHT-i));
-        if(!garlandIterator.hasNext()) {
-          throw new RuntimeException("Tree is broken!");
-        }
-        garlandBuilder.append(garlandIterator.next().getPresentation());
-      }
-      appendSpaces(garlandBuilder, height-i);
-    }
-    builder.append(garlandBuilder.reverse());
-    appendSpaces(builder, height - 1);
-    builder.append("|~|");
-    appendSpaces(builder, height - 1);
-    return builder.toString();
-  }
+		StringBuilder garlandBuilder = new StringBuilder();
+		Iterator<ISymbol> garlandIterator = garland.iterator();
+		for (int i = height; i > 0; i--) {
+			garlandBuilder.append(newLine);
+			appendSpaces(garlandBuilder, height - i);
+			for (int j = height - i; j <= i + height; j++) {
+				// System.out.println("j="+j+"; i="+i +
+				// "; space="+(TREE_HEIGHT-i));
+				if (!garlandIterator.hasNext()) {
+					throw new RuntimeException("Tree is broken!");
+				}
+				garlandBuilder.append(garlandIterator.next().getPresentation());
+			}
+			appendSpaces(garlandBuilder, height - i);
+		}
+		builder.append(reverseExcept(garlandBuilder, newLine));
+		appendSpaces(builder, height - 1);
+		builder.append("|~|");
+		appendSpaces(builder, height - 1);
+		return builder.toString();
+	}
 
-  private void appendSpaces(final StringBuilder garlandBuilder, final int space) {
-    if(space == 0) {
-      return;
-    }
-    garlandBuilder.append(" ");
-    appendSpaces(garlandBuilder, space-1);
-  }
+	private void appendSpaces(final StringBuilder garlandBuilder,
+			final int space) {
+		if (space == 0) {
+			return;
+		}
+		garlandBuilder.append(" ");
+		appendSpaces(garlandBuilder, space - 1);
+	}
+
+	/**
+	 * HOTFIX
+	 * 
+	 * @param builder
+	 * @return
+	 */
+	private StringBuilder reverseExcept(StringBuilder builder, String except) {
+		StringBuilder reversed = builder.reverse();
+		String delim = new StringBuilder(newLine).reverse().toString();
+		String fixed = reversed.toString().replaceAll(delim, newLine);
+		return new StringBuilder(fixed);
+	}
 
 }
